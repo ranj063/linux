58ec1a60a8043b3b5cfc8f3e8b0735238dcedc2d
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2022-03-03 20:49:28 -0800

    ASoC: SOF: Add a new op to get module info from firmware
    Add a new get_modules_info op in struct sof_ipc_fw_loader_ops
    to get the module information from the firmware and set the op for IPC4.
    This op is invoked after all the 3rd party module libraries have been
    loaded during topology loading.
    
    This patch is incomplete. We still need to parse the received data from
    the firmware.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- sound/soc/sof/sof-priv.h
+++ sound/soc/sof/sof-priv.h
@@ -382,6 +382,8 @@ struct sof_ipc_pm_ops {
  *			configuration from the booted firmware.
  *			Executed after the first successful firmware boot.
  * @load_library: Optional function pointer to load libraries for 3rd party modules
+ * @get_modules_info: Optional function pointer to retrieve the module information from the FW.
+ *		      This should be called after all 3rd party module libraries have been loaded.
  */
 struct sof_ipc_fw_loader_ops {
 	int (*validate)(struct snd_sof_dev *sdev);
@@ -389,6 +391,7 @@ struct sof_ipc_fw_loader_ops {
 	int (*load_fw_to_dsp)(struct snd_sof_dev *sdev);
 	int (*query_fw_configuration)(struct snd_sof_dev *sdev);
 	int (*load_library)(struct snd_sof_dev *sdev, guid_t *uuid);
+	int (*get_modules_info)(struct snd_sof_dev *sdev);
 };
 
 struct sof_ipc_tplg_ops;
